832f6ad8077ceca690c72eeb6d2f835447588dfb
praga6802 <praga.06.08@gmail.com>
2026-09-03 10:24:03 +0530

    new features

--- SmartJobPortalSystem/src/main/java/com/example/smartjobportalsystem/dto/AppPerJobCount.java
+++ SmartJobPortalSystem/src/main/java/com/example/smartjobportalsystem/dto/AppPerJobCount.java
@@ -1,15 +1,17 @@
 package com.example.smartjobportalsystem.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AppPerJobCount {
 
+    private Integer jobId;
     private String jobTitle;
+    private String companyName;
     private Long applicationCount;
 
-    public AppPerJobCount(String jobTitle, Long applicationCount) {
-        this.jobTitle = jobTitle;
-        this.applicationCount = applicationCount;
-    }
 }
--- SmartJobPortalSystem/src/main/java/com/example/smartjobportalsystem/repository/JobApplicationRepository.java
+++ SmartJobPortalSystem/src/main/java/com/example/smartjobportalsystem/repository/JobApplicationRepository.java
@@ -1,14 +1,19 @@
 package com.example.smartjobportalsystem.repository;
 
+import com.example.smartjobportalsystem.dto.AppPerCompanyCount;
+import com.example.smartjobportalsystem.dto.AppPerJobCount;
 import com.example.smartjobportalsystem.entity.Candidate;
 import com.example.smartjobportalsystem.entity.Company;
 import com.example.smartjobportalsystem.entity.JobApplication;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
@@ -19,4 +24,30 @@ public interface JobApplicationRepository extends JpaRepository<JobApplication, 
 
 
     List<JobApplication> findByJob_Company(Company company);
+
+
+    @Query("""
+            SELECT new com.example.smartjobportalsystem.dto.AppPerJobCount(
+                j.jobId,j.title,c.name,COUNT(a)
+            )
+            FROM JobApplication a
+            JOIN a.job j 
+            JOIN j.company c
+            GROUP BY j.jobId,j.title,c.name
+            """)
+    Optional<List<AppPerJobCount>> getApplicationsPerJob();
+
+
+    @Query("SELECT COUNT(a) from JobApplication a")
+    Long getTotalApplications();
+
+
+    @Query("""
+           SELECT new com.example.smartjobportalsystem.dto.AppPerCompanyCount(
+                c.companyId,c.name, COUNT(a)
+           )
+           FROM JobApplication a JOIN a.job j JOIN j.company c 
+           GROUP BY c.companyId, c.name
+            """)
+    Optional<List<AppPerCompanyCount>> getApplicationsPerCompany();
 }
